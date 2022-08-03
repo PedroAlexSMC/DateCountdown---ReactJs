2dee07b8d5ba374ae02b7282c21f679b2a1f2fdd
Pedro Alex <peupedroalex@yahoo.com.br>
2022-08-02 23:14:59 -0300

    First page implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/timerModule.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bgImage">
+      <div className="timeContainer">
+        <h1>Time Counter</h1>
+        <Component {...pageProps} />;
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
